283821d1c1d030a3d686e810f65e4c6ab58b3256
Maël DONNART <mael.donnart1@gmail.com>
2023-07-18 19:26:19 +0200

    feat(nvim): Fix treesitter errors

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,10 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    build = "<cmd>TSUpdate<CR>",
-    config = function ()
+    build = function()
+        local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+        ts_update()
+    end,
+    config = function()
         require("nvim-treesitter.configs").setup({
             enable = true,
             ensure_installed = "all",
