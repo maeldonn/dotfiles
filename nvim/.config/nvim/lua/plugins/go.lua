ab6ddcf19967971a602cd4821942b614aaec0edf
Maël DONNART <mael.donnart1@gmail.com>
2023-09-18 10:58:05 +0200

    fix(nvim): Move go stuff

--- nvim/.config/nvim/lua/plugins/go.lua
+++ nvim/.config/nvim/lua/plugins/go.lua
@@ -1,11 +1,19 @@
 return {
     {
         "ray-x/go.nvim",
+        config = function()
+            require("go").setup()
+        end,
+        event = { "CmdlineEnter" },
+        ft = { "go", "gomod" },
         build = function()
             require("go.install").update_all_sync()
         end
     },
     {
         "leoluz/nvim-dap-go",
+        keys = {
+            { "<leader>dt", function() require("dap-go").debug_test() end, desc = "Debug go test" }
+        }
     }
 }
